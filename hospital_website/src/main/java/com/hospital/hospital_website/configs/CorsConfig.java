0ec1925455ac22ae0368949f9bd3037be8530d41
MiesLies17-23 <vadimtolstouhov@gmail.com>
2025-11-28 23:55:13 +0300

    Изменил настройки для фото

--- hospital_website/src/main/java/com/hospital/hospital_website/configs/CorsConfig.java
+++ hospital_website/src/main/java/com/hospital/hospital_website/configs/CorsConfig.java
@@ -2,6 +2,7 @@ package com.hospital.hospital_website.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,16 @@ public class CorsConfig implements WebMvcConfigurer{
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/hospitalImages/**")
+                .addResourceLocations("file:C:/users/THUNDEROBOT/hospital_website/hospital_website/src/main/resources/static/images/hospitalImages/")
+                .setCachePeriod(0);
+
+        registry.addResourceHandler("/userImages/**")
+                .addResourceLocations("file:C:/users/THUNDEROBOT/hospital_website/hospital_website/src/main/resources/static/images/userImages/")
+                .setCachePeriod(0);
     }
 }
