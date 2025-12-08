b3206973724b708f47b722f2741f58bf761c48e3
MiesLies17-23 <vadimtolstouhov662@gmail.com>
2025-12-08 17:41:28 +0300

    Поменял структуру запросов и ответов

--- hospital_website/src/main/java/com/hospital/hospital_website/dto/AppointmentResponseDTO.java
+++ hospital_website/src/main/java/com/hospital/hospital_website/dto/AppointmentResponseDTO.java
@@ -1,13 +1,9 @@
 package com.hospital.hospital_website.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 @Getter
 @Setter
