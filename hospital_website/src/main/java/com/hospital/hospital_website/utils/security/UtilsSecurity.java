94dfbe52f46a5600df859a53a56c1c74ba22cfbe
MiesLies17-23 <vadimtolstouhov662@gmail.com>
2025-12-26 23:04:17 +0300

    Добавил документацию

--- hospital_website/src/main/java/com/hospital/hospital_website/utils/security/UtilsSecurity.java
+++ hospital_website/src/main/java/com/hospital/hospital_website/utils/security/UtilsSecurity.java
@@ -9,15 +9,30 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+/**
+ * Вспомогательный компонент для управления аутентификацией
+ */
 @Component
 public class UtilsSecurity {
+
+    /** Объект UserRepository для поиска пользователей */
     private static UserRepository userRepository;
 
+    /**
+     * Конструктор с параметром
+     *
+     * @param userRepository объект UserRepository
+     */
     @Autowired
     public UtilsSecurity (UserRepository userRepository) {
         UtilsSecurity.userRepository = userRepository;
     }
 
+    /**
+     * Возвращает имя пользователя из сессии или ошибку
+     *
+     * @return имя пользователя или ошибка
+     */
     public String getCurrentUsername() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated())
@@ -25,13 +40,22 @@ public class UtilsSecurity {
         return authentication.getName();
     }
 
-
+    /**
+     * Возвращает пользователя по имени из сессии
+     *
+     * @return данные пользователя или ошибка
+     */
     public User getCurrentUser() {
         String username = getCurrentUsername();
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new EntityNotFoundException("Ошибка поиска пользователя..."));
     }
 
+    /**
+     * Обновляет пользовательские данные в сессии
+     *
+     * @param user данные пользователя
+     */
     public void updateSecurityContext(User user) {
         Authentication currentAuth = SecurityContextHolder.getContext().getAuthentication();
 
--- hospital_website/src/main/java/com/hospital/hospital_website/utils/validation/UserParamsValidator.java
+++ hospital_website/src/main/java/com/hospital/hospital_website/utils/validation/UserParamsValidator.java
@@ -3,11 +3,22 @@ package com.hospital.hospital_website.utils.validation;
 import com.hospital.hospital_website.dto.request.UserCreateDTO;
 import com.hospital.hospital_website.exception.ValidateException;
 
+/**
+ * Вспомогательный компонент для валидации пользовательских данных
+ */
 public abstract class UserParamsValidator {
 
+    /** Шаблон для email'а пользователя */
     private static final String EMAIL_REGEX = "[a-zA-Z_]+\\w*@[a-zA-Z]+.[a-zA-Z]+";
+
+    /** Шаблон имени пользователя */
     private static final String USERNAME_REGEX = "[a-zA-Z_]+\\w{5,}";
 
+    /**
+     * Проверяет имя пользователя на соответствие шаблону
+     *
+     * @param username имя пользователя для проверки
+     */
     public static void usernameValidate(String username) {
         if (username.length() < 6)
             throw new ValidateException("Имя пользователя должно содержать не менее 6 символов");
@@ -17,16 +28,31 @@ public abstract class UserParamsValidator {
             throw new ValidateException("Имя пользователя содержит недопустимые символы!");
     }
 
+    /**
+     * Проверяет email пользователя на соответствие шаблону
+     *
+     * @param email email пользователя для проверки
+     */
     public static void emailValidate(String email) {
         if (!email.matches(EMAIL_REGEX))
             throw new ValidateException("Неправильный формат адреса электронной почты!");
     }
 
+    /**
+     * Проверяет пароль пользователя на допустимость
+     *
+     * @param password пароль пользователя для проверки
+     */
     public static void passwordValidate(String password) {
         if (password.length() < 6)
             throw new ValidateException("Пароль не может быть короче 6 символов!");
     }
 
+    /**
+     * Проверяет все пользовательские данные на допустимость
+     *
+     * @param userCreateDTO пользовательские данные для проверки
+     */
     public static void userParamsValidate(UserCreateDTO userCreateDTO) {
         UserParamsValidator.usernameValidate(userCreateDTO.getUsername());
         UserParamsValidator.emailValidate(userCreateDTO.getEmail());
